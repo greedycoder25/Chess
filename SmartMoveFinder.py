09cb37b5a2c70f1121c9e933677a69999a4dfd06
greedycoder25 <148771834+greedycoder25@users.noreply.github.com>
2024-10-19 23:31:20 +0530

    SmartMoveFinder.py

--- SmartMoveFinder.py
+++ SmartMoveFinder.py
@@ -8,36 +8,6 @@ DEPTH = 2
 def findRandomMoves(validMoves):
     return validMoves[random.randint(0,len(validMoves)-1)]
 
-def findBestMoveMinMaxNoRecursion(gs, validMoves):
-    turnMultiplier = 1 if gs.whiteToMove else -1
-    opponentMinMaxScore = CHECKMATE
-    bestPlayerMove = None
-    random.shuffle(validMoves)
-    for playerMove in validMoves:
-        gs.makeMove(playerMove)
-        opponentsMoves = gs.getValidMoves()
-        if gs.staleMate:
-            opponentMaxScore = STALEMATE
-        elif gs.checkMate:
-            opponentMaxScore = -CHECKMATE
-        else:
-            opponentMaxScore = -CHECKMATE
-            for opponentsMove in opponentsMoves:
-                gs.makeMove(opponentsMove)
-                if gs.checkMate:
-                    score = CHECKMATE
-                elif gs.staleMate:
-                    score = STALEMATE
-                else:
-                    score  = -turnMultiplier * scoreMaterial(gs.board)
-                if (score > opponentMaxScore):
-                    opponentMaxScore = score
-                gs.undoMove()
-        if opponentMaxScore < opponentMinMaxScore:
-            opponentMinMaxScore = opponentMaxScore
-            bestPlayerMove = playerMove
-        gs.undoMove()
-    return bestPlayerMove
 
 def findBestMove(gs, validMoves):
     global nextMove,counter
@@ -45,40 +15,8 @@ def findBestMove(gs, validMoves):
     random.shuffle(validMoves)
     counter =0
     findMoveNegaMax(gs, validMoves, DEPTH, 1 if gs.whiteToMove else -1)
-    #findMoveNegaMaxAlphaBeta(gs, validMoves, DEPTH, -CHECKMATE, CHECKMATE, 1 if gs.whiteToMove else -1)
     print(counter)
     return nextMove
-
-
-def findMoveMinMax(gs,validMoves, depth, whiteToMove):
-    global nextMove
-    if depth == 0:
-        return scoreMaterial(gs.board)
-    if whiteToMove:
-        maxScore = -CHECKMATE
-        for move in validMoves:
-            gs.makeMove(move)
-            nextMoves = gs.getValidMoves()
-            score = findMoveMinMax(gs,nextMoves,depth-1, False)
-            if score > maxScore:
-                maxScore = score
-                if depth == DEPTH:
-                    nextMove = move
-            gs.undoMove()
-        return maxScore
-            
-    else:
-        minScore = CHECKMATE
-        for move in validMoves:
-            gs.makeMove(move)
-            nextMoves = gs.getValidMoves()
-            score = findMoveMinMax(gs,nextMoves,depth-1, True)
-            if score < minScore:
-                minScore = score
-                if depth == DEPTH:
-                    nextMove = move
-            gs.undoMove()
-        return minScore
 
 
 def findMoveNegaMax(gs, validMoves, depth, turnMultiplier):
@@ -101,28 +39,6 @@ def findMoveNegaMax(gs, validMoves, depth, turnMultiplier):
     return maxScore
 
 
-def findMoveNegaMaxAlphaBeta(gs, validMoves, depth, alpha, beta, turnMultiplier):
-    global nextMove, counter
-    counter +=1
-    if depth  == 0:
-        return turnMultiplier * scoreBoard(gs)
-    
-    #
-    maxScore = -CHECKMATE
-    for move in validMoves:
-        gs.makeMove(move)
-        nextMoves = gs.getValidMoves()
-        score = -findMoveNegaMaxAlphaBeta(gs,nextMoves, depth - 1, -beta, -alpha, -turnMultiplier )
-        if score > maxScore:
-            maxScore = score
-            if depth == DEPTH:
-                nextMove = move
-        gs.undoMove()
-        if maxScore > alpha:
-            alpha = maxScore
-        if alpha >= beta:
-            break
-    return maxScore
 '''
 A Positive score is good for white, a negative score is good for black
 '''
